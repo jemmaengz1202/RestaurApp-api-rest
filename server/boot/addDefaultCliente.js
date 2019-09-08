ed82525c35038790dfd21056ff530610e2bf4b9a
jemmaengz <juanemmanuel1202@gmail.com>
2019-09-07 21:58:26 -0500

    Add addDefaultCliente.js boot script
    Added named script to create a default Cliente.
     En la rama feature-add-default-client
     Cambios para hacer commit:
    	new file:   server/boot/addDefaultCliente.js

---
+++ server/boot/addDefaultCliente.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/*
+  Crear cliente por default, con el id 1 y que será utilizado para todas las órdenes no registradas.
+*/
+module.exports = function(app, cb) {
+  const Cliente     = app.models.Cliente;
+
+  Cliente.findById(1, (err, cliente) => {
+      if (!cliente) {
+          Cliente.create({
+            rfc: "",
+            nombre: "Cliente",
+            direccion: "",
+            colonia: "",
+            ciudad: "",
+            cp: "",
+            telefono: ""
+          }, (err) => {
+              if (err) return cb(err);
+              console.log('Cliente por defecto (con id:1) creado satisfactoriamente');
+            }
+          );
+      }
+      if(err) cb(err);
+  });
+
+  process.nextTick(cb);
+};
