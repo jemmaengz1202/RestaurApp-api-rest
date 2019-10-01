e4b12d6d991de066270a2219ef5f80e132f63d1b
jemmaengz <juanemmanuel1202@gmail.com>
2019-10-01 07:20:23 -0500

    Add cookie support for access tokens

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Usuario) {
-
+  Usuario.afterRemote('login', function(ctx) {
+    ctx.res.cookie('access_token', ctx.result.id, {
+      signed: true,
+      maxAge: ctx.result.ttl * 1000
+    });
+    return Promise.resolve();
+  });
+  Usuario.afterRemote('logout', function(ctx) {
+    ctx.res.clearCookie('access_token');
+    return Promise.resolve();
+  });
 };
---
+++ server/config.local.js
@@ -0,0 +1,7 @@
+'use strict';
+
+require('dotenv').config();
+
+module.exports = {
+  cookieSecret: process.env.COOKIE_SECRET,
+};
