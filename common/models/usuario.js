587e08c8611685850f37b9c8d1319228d25448fb
jemmaengz <juanemmanuel1202@gmail.com>
2019-10-04 09:49:54 -0500

    Fix cookies max age when ttl = -1  En la rama master  Cambios para hacer commit: 	modificado: common/models/usuario.js

--- common/models/usuario.js
+++ common/models/usuario.js
@@ -4,8 +4,14 @@ module.exports = function(Usuario) {
   Usuario.afterRemote('login', function(ctx) {
     ctx.res.cookie('access_token', ctx.result.id, {
       signed: true,
-      maxAge: ctx.result.ttl * 1000
+      maxAge: ctx.result.ttl * 1000,
     });
+    if (ctx.result.ttl == -1) {
+      ctx.res.cookie('access_token', ctx.result.id, {
+        signed: true,
+        maxAge: 21474836470,
+      });
+    }
     return Promise.resolve();
   });
   Usuario.afterRemote('logout', function(ctx) {
