285d82d266e03a24b92690010cf3b85dafef0298
jemmaengz <juanemmanuel1202@gmail.com>
2019-10-23 23:59:41 -0500

    Add 'setPreparada' remote method

--- common/models/orden.js
+++ common/models/orden.js
@@ -1,5 +1,31 @@
 'use strict';
 
 module.exports = function(Orden) {
-
+  /**
+ * Sets 'preparada' property to true
+ */
+  Orden.prototype.setPreparada = async function() {
+    const orden = this;
+    const savePreparada = (orden) => {
+      return new Promise(
+        (resolve, reject) => {
+          orden.preparada = true;
+          orden.save((err, obj) => {
+            if (err) reject(err);
+            resolve(obj);
+          });
+        }
+      );
+    };
+    try {
+      await savePreparada(orden);
+      return {
+        ok: true,
+      };
+    } catch (err) {
+      return {
+        error: err,
+      };
+    }
+  };
 };
