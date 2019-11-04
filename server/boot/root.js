242aa6e87373150c2b80bb85fbf723995a7d10a3
Juan Enríquez <jemmaengz1202@gmail.com>
2019-11-03 20:08:18 -0600

    Modify /api/usuarios/whoami to include role

--- server/boot/root.js
+++ server/boot/root.js
@@ -2,24 +2,82 @@
 
 module.exports = function(server) {
   const router = server.loopback.Router();
-  router.get('/api/usuarios/whoami', (req, res) => {
+  router.get('/api/usuarios/whoami', async (request, response) => {
     const app = require('../server');
+    const Role = app.models.Role;
+    const RoleMapping = app.models.RoleMapping;
     const AccessToken = app.models.AccessToken;
-    AccessToken.findForRequest(req, {}, function(_, accesstoken) {
-      if (accesstoken == undefined) {
-        res.status(401);
-        res.send({
-          error: 'Unauthorized',
-          message: 'You need to be authenticated to access this endpoint'
+    const UserModel = app.models.Usuario;
+
+    const promiseRol = (userId) => {
+      return new Promise((resolve, reject) => {
+        Role.getRoles(
+          {principalType: RoleMapping.USER, principalId: userId},
+          (err, roles) => {
+            if (err) reject(err);
+            if (roles.includes(1)) {
+              resolve('admin');
+            } else if (roles.includes(2)) {
+              resolve('mesero');
+            } else if (roles.includes(3)) {
+              resolve('cocinero');
+            } else {
+              resolve('none');
+            }
+          }
+        );
+      });
+    };
+
+    const promiseAccesTokenUserId = (req) => {
+      return new Promise((resolve, reject) => {
+        AccessToken.findForRequest(req, {}, (_, accesstoken) => {
+          if (accesstoken == undefined) {
+            reject({
+              err: 'Token not found',
+            });
+          } else {
+            resolve(accesstoken.userId);
+          }
         });
-      } else {
-        const UserModel = app.models.Usuario;
-        UserModel.findById(accesstoken.userId, function(_, user) {
-          res.status(200);
-          res.send(user);
+      });
+    };
+
+    const promiseUser = (userId) => {
+      return new Promise((resolve, reject) => {
+        UserModel.findById(userId, (err, user) => {
+          if (err) {
+            reject({
+              err: 'User not found',
+            });
+          }
+          if (user) resolve(user);
         });
-      }
-    });
+      });
+    };
+
+    try {
+      const userId = await promiseAccesTokenUserId(request);
+      const user = await promiseUser(userId);
+      const role = await promiseRol(userId);
+
+      const {
+        realm,
+        password,
+        emailVerified,
+        verificationToken,
+        ...userToSend
+      } = user.__data;
+
+      response.status(200);
+      response.send({
+        ...userToSend,
+        role
+      });
+    } catch (err) {
+      response.status(404);
+      response.send(err);
+    }
   });
   server.use(router);
 };
